2c9bf96d508a7aabd80e4d23d81574f7dbae7c02
dvsni <danil.shir@outlook.com>
2023-01-25 13:56:45 +0300

    contact

--- contact.py
+++ contact.py
@@ -1,3 +1,16 @@
 from csv_create import creating
 from csv_write import csv_writing
 
+def greeting():
+    print("Телефонный справочник!")
+def get_info():
+    info = []
+    Last_name = input('Введите фамилию: ')
+    info.append(Last_name)
+    Name = input('Введите имя: ')
+    info.append(Name)
+    phone_number = input('Укажите телефон: ')
+    info.append(phone_number)
+    Comment = input('Укажите комментарий: ')
+    info.append(Comment)
+    return [Last_name, Name, phone_number, Comment]
